64f68086649199b7e17e508d3fc1c00beb13113e
amanda <amanda.piva01@fatec.sp.gov.br>
2022-10-13 10:36:42 -0300

    Criando um mock de estudantes

--- src/app/estudantes/estudantes.component.ts
+++ src/app/estudantes/estudantes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { student } from '../student';
+import { STUDENT } from '../mock-studant';
 
 @Component({
   selector: 'app-estudantes',
@@ -8,12 +9,7 @@ import { student } from '../student';
 })
 export class EstudantesComponent implements OnInit {
 
-  student: student = {
-    id: 5124,
-    name: 'Ana Carolina Machado',
-    data: 22032006,
-    serie: 'Segundo ano do ensino médio'
-  };
+  student = STUDENT;
 
   constructor() { }
 
---
+++ src/app/mock-studant.ts
@@ -0,0 +1,14 @@
+import { student } from './student';
+
+export const STUDENT: student[] = [
+    { id: 1254, name: 'João Barros da Silva', data: 5102007, serie:"Primeiro ano do ensino médio" },
+    { id: 2284, name: 'Maria Luiza dos Santos', data: 14082007, serie:"Primeiro ano do ensino médio" },
+    { id: 3294, name: 'Nicole Dantas Frederic', data: 16092006, serie:"Segundo ano do ensino médio" },
+    { id: 4254, name: 'Katy Barbosa Cardoso', data: 22062005, serie:"Terceiro ano do ensino médio" },
+    { id: 5294, name: 'Beatriz Matos', data: 7092007, serie:"Primeiro ano do ensino médio" },
+    { id: 6254, name: 'Carlos Santos Prado', data: 30112005, serie:"Terceiro ano do ensino médio" },
+    { id: 7254, name: 'Laura Maranto Nascimento', data: 25022006, serie:"Segundo ano do ensino médio" },
+    { id: 8254, name: 'Lucas Amaral', data: 9032006, serie:"Segundo ano do ensino médio" },
+    { id: 9254, name: 'Mariana Galante Matos', data: 20012007, serie:"Primeiro ano do ensino médio" },
+    { id: 10254, name: 'Heitor Barros de Souza', data: 8052005, serie:"Terceiro ano do ensino médio" },
+  ];
