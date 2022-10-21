a190ebfc90f19b04a4e069ada198a4f12b3fb748
amanda <amanda.piva01@fatec.sp.gov.br>
2022-10-21 09:16:15 -0300

    Finalização do projeto, correção de bugs e editar alunos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { EstudantesComponent } from './estudantes/estudantes.component';
-import { StudentDetailComponent } from './student-detail/student-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstudantesComponent,
-    StudentDetailComponent
+    EstudantesComponent
   ],
   imports: [
     BrowserModule,
